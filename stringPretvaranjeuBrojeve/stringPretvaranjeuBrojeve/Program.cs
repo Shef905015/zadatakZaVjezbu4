31857857c76f5ac195e8a50ee1e041a7492ce6b6
Non't <90344104+Shef905015@users.noreply.github.com>
2023-11-08 09:30:15 +0100

    Add files via upload

---
+++ stringPretvaranjeuBrojeve/stringPretvaranjeuBrojeve/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringPretvaranjeuBrojeve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string recenica;
+            string x;
+            string y;
+
+            Console.WriteLine("Unesite jedan decimalni i jedan cijeli broj sa razmakom: ");
+            recenica = Console.ReadLine();
+
+            x = recenica.Substring(0, recenica.IndexOf(" "));
+            y = recenica.Substring(recenica.LastIndexOf(" ") + 1);
+
+            Console.WriteLine(x + " " + y);
+
+            int x2 = Convert.ToInt32(x);
+            double y2 = Convert.ToDouble(y);
+
+            Console.WriteLine(x2 * 2);
+            Console.WriteLine(y2 * 2);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
